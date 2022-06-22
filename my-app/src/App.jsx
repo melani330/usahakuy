893bd4719db15b6be440c88246ebebbc67fe6691
apiiiiip <apip.afif46@gmail.com>
2022-06-22 23:10:34 +0700

    feat: adding page pelatihan

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,20 +1,22 @@
 import * as React from "react";
-import Footer from "./components/footer";
-import Navbar from "./components/Navbar";
-import Tentang from "./components/tentang";
-import Pendaftaran from "./components/pendaftaran";
-import data from "./data/pendaftaran.json";
+// import Footer from "./components/footer";
+// import Navbar from "./components/Navbar";
+// import Tentang from "./components/tentang";
+// import Pendaftaran from "./components/pendaftaran";
+// import data from "./data/pendaftaran.json";
+import Pelatihan from "./Pages/Pelatihan";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar />
       <Tentang />
       <Pendaftaran data={data} />
       <div>
         <Tentang />
       </div>
-      <Footer />
+      <Footer /> */}
+      <Pelatihan />
     </div>
   );
 }
